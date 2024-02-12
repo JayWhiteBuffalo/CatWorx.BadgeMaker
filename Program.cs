e75230488d17c27ed502aa6821242cf264292ff5
Mason <TylerEMason@proton.me>
2024-02-12 07:29:23 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
+            
+        }
+
+        static List<Employee> GetEmployees(){
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: ");
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == ""){
+                    break;
+                }
+
+                Console.WriteLine("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+
+                Console.WriteLine("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+
+                Console.WriteLine("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine() ?? "";
+
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+        static void PrintEmployees(List<Employee> employees) {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
+        }
+    }
+}
